0662a1c54e3bfa198078f71e36fb40a554e16d89
anajd <ana.jaime.diaz@gmail.com>
2019-04-08 09:40:15 +0200

    nope

---
+++ js/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const numberInput = document.getElementById("addNumber");
+const btn = document.querySelector(".game__btn");
+const feedBack = document.querySelector(".feedback");
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+console.log("> " + getRandomNumber(100));
+
+const newNumber = () => {
+  console.log(numberInput.value);
+  const value = numberInput.value;
+  if (value === getRandomNumber) {
+    return (feedBack.innerHTML = "¡HAS GANADO CAMPEONA!");
+  } else if (value < getRandomNumber) {
+    return (feedBack.innerHTML = "Demasiado bajo");
+  } else if (value > getRandomNumber) {
+    return (feedBack.innerHTML = "Demasiado alto");
+  }
+};
+
+btn.addEventListener("click", newNumber);
